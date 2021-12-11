b335858e59d02b13b940fd72a185c57526ea39cf
Denedo <tegajoseph2002@gmail.com>
2021-12-11 18:07:48 +0100

    set up react query

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React, { Suspense } from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import * as Sentry from "@sentry/react";
 import { Integrations } from "@sentry/tracing";
-import App from './App';
-import UserProvider from './components/providers/AuthProvider'
-import CartProvider from './components/providers/CartProvider';
-import * as serviceWorker from './serviceWorker';
-import './index.css';
-import ScreenLoader from './components/common/ScreenLoader';
+import App from "./App";
+import UserProvider from "./components/providers/AuthProvider";
+import CartProvider from "./components/providers/CartProvider";
+import * as serviceWorker from "./serviceWorker";
+import "./index.css";
+import ScreenLoader from "./components/common/ScreenLoader";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 Sentry.init({
   dsn: "https://d9c11e37b49d47fcb4b6c74a3deb06ed@o684222.ingest.sentry.io/5771452",
@@ -22,15 +25,18 @@ Sentry.init({
 
 ReactDOM.render(
   <Suspense fallback={<ScreenLoader />}>
-    <UserProvider>
-      <CartProvider>
-        <Router>
-          <App />
-        </Router>
-      </CartProvider>
-    </UserProvider>
+    <QueryClientProvider client={queryClient}>
+      <UserProvider>
+        <CartProvider>
+          <Router>
+            <App />
+          </Router>
+        </CartProvider>
+      </UserProvider>
+    </QueryClientProvider>
   </Suspense>,
-  document.getElementById('root'));
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
